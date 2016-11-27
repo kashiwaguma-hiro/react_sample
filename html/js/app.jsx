b982f6dab05b4fc91aeec7a16f52af8f14da48b1
kashiwaguma-hiro <hard2259@gmail.com>
2016-11-28 00:13:33 +0900

    [update] todo list

--- html/js/app.jsx
+++ html/js/app.jsx
@@ -1,27 +1,99 @@
-class Counter extends React.Component {
-    constructor() {
-        this.state = {
-            count: 0
-        };
-    }
+class TodoApp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.updateDone = this.updateDone.bind(this);
+    this.state = {
+      items: [],
+      newTodoLabel: ''
+    };
+  }
 
-    onClick() {
-        this.setState({
-            count: this.state.count + 1
-        });
-    }
+  render() {
+    return (
+      <div>
+        <h3>TODO</h3>
+        <TodoList items={this.state.items} onUpdateDone={this.updateDone}/>
+        <form onSubmit={this.handleSubmit}>
+          <input onChange={this.handleChange} value={this.state.newTodoLabel} />
+          <button>{'Add #' + (this.state.items.length + 1)}</button>
+        </form>
+      </div>
+    );
+  }
 
-    render() {
-        return (
-            <div>
-                <h1>{this.state.count}</h1>
-                <button onClick={this.onClick.bind(this)}>Count Up!</button>
-            </div>
-        )
-    }
+  handleChange(e) {
+    this.setState({newTodoLabel: e.target.value});
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    var newItem = {
+      id: Date.now(),
+      label: this.state.newTodoLabel,
+      done: false
+    };
+
+    this.setState((prevState) => ({
+      items: prevState.items.concat(newItem),
+      newTodoLabel: ''
+    }));
+  }
+
+  updateDone(id, done){
+    var targetItem = this.state.items.filter((item) => {
+        return item.id == id;
+    })[0];
+
+    targetItem.done = done;
+
+    this.setState({
+      items: this.state.items.concat(targetItem)
+    });
+  }
 }
 
-React.render(
-    <Counter />,
+class TodoList extends React.Component {
+  render() {
+      return (
+        <ol>
+          {this.props.items.map(item => (
+            <TodoItem key={item.id} item={item} onUpdateDone={this.props.onUpdateDone}/>
+          ))}
+        </ol>
+      );
+  }
+}
+
+class TodoItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this._onUpdateDone = this._onUpdateDone.bind(this);
+  }
+
+  // delegate updateed done flag events.
+  _onUpdateDone(e){
+    this.props.onUpdateDone(this.props.item.id, e.target.checked);
+  }
+
+  render() {
+    const label = this.props.item.label;
+    const done = this.props.item.done;
+
+    return (
+      <li className={done ? 'completed': ''}>
+        <label>
+          <input type="checkbox" checked={done ? 'checked': ''} onChange={this._onUpdateDone}/>
+          {label}
+        </label>
+      </li>
+    );
+  }
+
+}
+
+ReactDOM.render(
+    <TodoApp />,
     document.getElementById('app-container')
 );
