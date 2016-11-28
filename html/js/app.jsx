df2e0d2cb431a76918dbe05998f25135156fa838
kashiwaguma-hiro <hard2259@gmail.com>
2016-11-29 01:26:52 +0900

    [update] change todo list app

--- html/js/app.jsx
+++ html/js/app.jsx
@@ -4,17 +4,19 @@ class TodoApp extends React.Component {
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.updateDone = this.updateDone.bind(this);
+    this.delete = this.delete.bind(this);
     this.state = {
-      items: [],
+      items: [], // TODO convert to Map
       newTodoLabel: ''
     };
   }
 
   render() {
+    console.log(this.state.items);
     return (
       <div>
         <h3>TODO</h3>
-        <TodoList items={this.state.items} onUpdateDone={this.updateDone}/>
+        <TodoList items={this.state.items} onUpdateDone={this.updateDone} onDelete={this.delete}/>
         <form onSubmit={this.handleSubmit}>
           <input onChange={this.handleChange} value={this.state.newTodoLabel} />
           <button>{'Add #' + (this.state.items.length + 1)}</button>
@@ -41,27 +43,37 @@ class TodoApp extends React.Component {
     }));
   }
 
-  updateDone(id, done){
+  updateDone(targetId, newDone){
     var targetItem = this.state.items.filter((item) => {
-        return item.id == id;
+        return item.id == targetId;
     })[0];
 
-    targetItem.done = done;
+    // targetItem.done = newDone;
 
     this.setState({
-      items: this.state.items.concat(targetItem)
-    });
+      items: React.addons.update(this.state.items, { targetId : { $set: newDone }})
+    })
+    // this.setState(
+    //   {items:this.state.items.concat(targetItem)}
+    // )
+  }
+
+  delete(id){
+    this.setState({
+      items: this.state.items.filter((item) => {
+        return item.id != id;
+    })});
   }
 }
 
 class TodoList extends React.Component {
   render() {
       return (
-        <ol>
+        <ul>
           {this.props.items.map(item => (
-            <TodoItem key={item.id} item={item} onUpdateDone={this.props.onUpdateDone}/>
+            <TodoItem key={item.id} item={item} onUpdateDone={this.props.onUpdateDone} onDelete={this.props.onDelete}/>
           ))}
-        </ol>
+        </ul>
       );
   }
 }
@@ -70,11 +82,16 @@ class TodoItem extends React.Component {
   constructor(props) {
     super(props);
     this._onUpdateDone = this._onUpdateDone.bind(this);
+    this._onDelete = this._onDelete.bind(this);
   }
 
   // delegate updateed done flag events.
   _onUpdateDone(e){
     this.props.onUpdateDone(this.props.item.id, e.target.checked);
+  }
+
+  _onDelete(){
+    this.props.onDelete(this.props.item.id);
   }
 
   render() {
@@ -86,6 +103,7 @@ class TodoItem extends React.Component {
         <label>
           <input type="checkbox" checked={done ? 'checked': ''} onChange={this._onUpdateDone}/>
           {label}
+          <input type="button" onClick={this._onDelete} value="DEL"/>
         </label>
       </li>
     );
